6867656c066f9f6128acd3023f33743556d79d8a
chenyarong <chenyarong@haochang.tv>
2017-08-28 11:04:19 +0800

    init

--- public/views/home/index.vue
+++ public/views/home/index.vue
@@ -8,16 +8,33 @@
 </template>
 
 <script>
+var mqtt = require('mqtt');
 export default {
     data() {
         return {
             message: '',
             box: '',
+            client: '',
         }
+    },
+    mounted: function() {
+        //var client = mqtt.connect('http://127.0.0.1:15675/ws');
+        let self = this;
+        self.client = mqtt.connect('http://127.0.0.1:15675/ws');
+        self.client.on('connect', function() {
+            self.client.subscribe('hello/demo');
+            self.client.publish('hello/demo', "I'm connected!");
+        });
+
+        self.client.on('message', function(topic, message) {
+            self.box += message.toString() + "<br />";
+        });
+
     },
     methods: {
         push() {
-            this.box += this.message + "<br />"; 
+            // this.box += this.message + "<br />"; 
+            this.client.publish('hello/demo', this.message);
         },
     }
 }
